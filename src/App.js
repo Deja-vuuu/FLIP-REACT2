c01729f238c7f218da0bd592f6c920b2ac1b1ca3
杨阳 <1002422146@qq.com>
2020-10-10 15:27:48 +0800

    feat: 实现基本功能

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react';
 import 'web-animations-js';
 import './App.scss'
-export default class AutoFocusInput extends Component {
-  listRef= React.createRef();
+export default class FLIP extends Component {
 
+  listRef= React.createRef();
+  activeCardList =[]
   state = {
     initCardNum: 3,
     cardList: [],
@@ -18,46 +19,62 @@ export default class AutoFocusInput extends Component {
   getArrByLen=(len) => {
     return Array(len).fill().map(() => [0, 0]);
   }
-  add = ()=>{
+  /**
+   * 新增卡片
+   * @param num 新增卡片数量 默认新增一个
+   */
+  add = (num = 1)=>{
     let  {cardList} = this.state;
-    let list = this.listRef.current;
-    let transArr=[];
-    let currentCardList = Array.prototype.slice.call(this.listRef.current.children);
-    transArr = this.getArrByLen(currentCardList.length);
+    let cardListRef = this.listRef.current;
+    let currentCardList = Array.prototype.slice.call(cardListRef.children);
+    this.transArr = this.getArrByLen(currentCardList.length);
     currentCardList.forEach((value, index) => {
       let rectInfo = value.getBoundingClientRect();
-      transArr[index][0] = rectInfo.left;
-      transArr[index][1] = rectInfo.top;
+      this.transArr[index].left = rectInfo.left;
+      this.transArr[index].top = rectInfo.top;
     });
-    this.transArr = transArr
-    this.newTransArr = transArr;
-    cardList.unshift({
-       index: 1
-    })
+    let addList = this.getArrByLen(num)
+    let newCardList = cardList.concat(addList)
+    this.addNum = num
     this.setState(
         {
-          cardList:cardList,
+          cardList:newCardList,
           animateStatus: 1,
         }
     );
   }
-  componentDidUpdate(){
+  /**
+   * 删除卡片
+   * @param delIndex 删除卡片的索引
+   */
+  del = (delIndex)=>{
+    let  {cardList} = this.state;
+    let cardListRef = this.listRef.current;
+    let currentCardList = Array.prototype.slice.call(cardListRef.children).slice(delIndex+1);
+    this.transArr = this.getArrByLen(currentCardList.length + 1);
+    currentCardList.forEach((value, index) => {
+      let rectInfo = value.getBoundingClientRect();
+      this.transArr[index+1].left = rectInfo.left;
+      this.transArr[index+1].top = rectInfo.top;
+    });
+    let newCardList = cardList.filter((value, index) => index !== delIndex)
+    this.delIndex = delIndex
+    this.setState(
+        {
+          cardList:newCardList,
+          animateStatus: 2,
+        }
+    );
+  }
+  componentDidUpdate(prevProps, prevState){
     if(this.state.animateStatus === 1){
-      let transArr =[];
-      let currentCardList = Array.prototype.slice.call(this.listRef.current.children).slice();
-      currentCardList = currentCardList.slice(1)
-      console.log('currentCardList',currentCardList)
-      transArr = this.getArrByLen(currentCardList.length);
-      currentCardList.forEach((value, index) => {
+      let currentCardList = Array.prototype.slice.call(this.listRef.current.children);
+      // 位置需要进行改变的节点（可以大致认为位于当前变化节点之前的所有节点，位置完全不变）
+      let activeCardList = currentCardList.slice(this.addNum)
+      activeCardList.forEach((value, index) => {
         let rectInfo = value.getBoundingClientRect();
-        // console.log(this.newTransArr[index],rectInfo.left)
-        // console.log(this.newTransArr[index][0])
-        console.log(this.transArr[index])
-        console.log([rectInfo.left,rectInfo.top])
-        transArr[index][0] = rectInfo.left;
-        transArr[index][1] = rectInfo.top;
-        let invertLeft = this.transArr[index][0] - rectInfo.left
-        let invertTop = this.transArr[index][1] - rectInfo.top
+        let invertLeft = this.transArr[index].left - rectInfo.left
+        let invertTop = this.transArr[index].top - rectInfo.top
         const keyframes = [
           {
             transform: `translate(${invertLeft}px, ${invertTop}px)`,
@@ -68,21 +85,37 @@ export default class AutoFocusInput extends Component {
           duration: 1000,
           easing: "cubic-bezier(0,0,0.32,1)",
         }
-        const animation = value.animate(keyframes, options)
-      });
-      this.setState({
-        animateStatus: 2
+        value.animate(keyframes, options)
       });
     }
     if(this.state.animateStatus === 2){
-      // requestAnimationFrame(() => {
-      //   this.setState({
-      //     animateStatus: 3
-      //   });
-      // });
+      let currentCardList = Array.prototype.slice.call(this.listRef.current.children);
+      currentCardList = currentCardList.slice(this.delIndex)
+      // 位置需要进行改变的节点（可以大致认为位于当前变化节点之前的所有节点，位置完全不变）
+      currentCardList.forEach((value, index) => {
+        let rectInfo = value.getBoundingClientRect();
+        let invertLeft = this.transArr[index+1].left - rectInfo.left
+        let invertTop = this.transArr[index+1].top - rectInfo.top
+        const keyframes = [
+          {
+            transform: `translate(${invertLeft}px, ${invertTop}px)`,
+          },
+          { transform: "translate(0)" },
+        ]
+        const options = {
+          duration: 1000,
+          easing: "cubic-bezier(0,0,0.32,1)",
+        }
+        value.animate(keyframes, options)
+      });
     }
   }
   componentDidMount() {
+    let a = Array(3).fill({
+      a:1,
+      b:2
+    });
+    console.log(a)
     let cardList = this.creatCardList();
     this.setState(
         {
@@ -96,15 +129,27 @@ export default class AutoFocusInput extends Component {
   render() {
     let { cardList } = this.state;
     return <div className='page'>
-      <div onClick={this.add}>
+      <div onClick={()=>{
+        this.add(1)
+      }}>
+        新增
+      </div>
+      <div onClick={()=>{
+        this.add(2)
+      }}>
         新增
       </div>
       <div className='card-container' ref={this.listRef}>
         {
           cardList.map((value,index)=>{
-            return  <div className='card-item' id={index} key={index} style={{
+            return  <div className='card-item' id={index} key={index}
+                         style={{
               zIndex: 9999-index
-            }}>
+            }}
+            onClick={()=>{
+              this.del(index)
+            }}
+            >
               <div className='title'>
                 {
                   index
