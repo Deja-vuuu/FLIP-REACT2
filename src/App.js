ddbe1c5e43aa8ecec89e6d3476c14631d2496602
杨阳 <1002422146@qq.com>
2020-10-12 18:16:29 +0800

    feat: 走起来

--- src/App.js
+++ src/App.js
@@ -52,23 +52,15 @@ export default class FLIP extends Component {
   del = (delIndex)=>{
     let  {cardList} = this.state;
     let cardListRef = this.listRef.current;
-    let currentCardList = Array.prototype.slice.call(cardListRef.children).slice(delIndex+1);
-    this.activeCardList = Array.prototype.slice.call(cardListRef.children).slice(delIndex)
-    Array.prototype.slice.call(cardListRef.children).forEach((value, index) => {
-      let rectInfo = value.getBoundingClientRect();
-      console.log('rectInfo',rectInfo.left,rectInfo.top)
-    });
-    console.log('this.Array.prototype.slice.call(cardListRef.children)',Array.prototype.slice.call(cardListRef.children))
-    console.log('this.activeCardList',this.activeCardList)
-
-
-    this.transArr = this.getArrByLen( this.activeCardList.length);
-    this.activeCardList.forEach((value, index) => {
+    let activeCardList = Array.prototype.slice.call(cardListRef.children).slice(delIndex)
+    console.log('activeCardList',activeCardList)
+    this.transArr = this.getArrByLen(this.activeCardList.length);
+    activeCardList.forEach((value, index) => {
       let rectInfo = value.getBoundingClientRect();
       this.transArr[index].left = rectInfo.left;
       this.transArr[index].top = rectInfo.top;
     });
-    console.error(' this.transArr this.transArr this.transArr', this.transArr)
+    console.log(' this.transArr', this.transArr)
     let newCardList = cardList.filter((value, index) => index !== delIndex)
     this.delIndex = delIndex
     this.setState(
@@ -79,59 +71,42 @@ export default class FLIP extends Component {
     );
   }
   componentDidUpdate(prevProps, prevState){
-    if(this.state.animateStatus === 1){
-      let currentCardList = Array.prototype.slice.call(this.listRef.current.children);
-      // 位置需要进行改变的节点（可以大致认为位于当前变化节点之前的所有节点，位置完全不变）
-      let activeCardList = currentCardList.slice(this.addNum)
-      activeCardList.forEach((value, index) => {
-        let rectInfo = value.getBoundingClientRect();
-        let invertLeft = this.transArr[index].left - rectInfo.left
-        let invertTop = this.transArr[index].top - rectInfo.top
-        const keyframes = [
-          {
-            transform: `translate(${invertLeft}px, ${invertTop}px)`,
-          },
-          { transform: "translate(0)" },
-        ]
-        const options = {
-          duration: 1000,
-          easing: "cubic-bezier(0,0,0.32,1)",
-        }
-        value.animate(keyframes, options)
-      });
+    const currentCardList = Array.prototype.slice.call(this.listRef.current.children);
+    const {animateStatus} = this.state;
+    let activeCardList=[];
+    // 位置需要进行改变的节点（当前变化节点之前的所有节点，位置完全不变）
+    switch (animateStatus){
+      case 1:
+        activeCardList = currentCardList.slice(this.addNum)
+        break;
+      case 2:
+        activeCardList = currentCardList.slice(this.delIndex)
+        break;
+      // default:
+      //   break;
     }
-    if(this.state.animateStatus === 2){
-      console.log(' Array.prototype.slice.call(this.listRef.current.children)', Array.prototype.slice.call(this.listRef.current.children))
-      let a = Array.prototype.slice.call(this.listRef.current.children).slice(this.delIndex);
-      console.log('a',a)
-      console.log('this.activeCardList',this.activeCardList)
-      let currentCardList = this.activeCardList
-      console.log('currentCardList',currentCardList, this.transArr)
-      // 位置需要进行改变的节点（可以大致认为位于当前变化节点之前的所有节点，位置完全不变）
-      this.activeCardList.forEach((value, index) => {
-        let rectInfo = value.getBoundingClientRect();
-        let invertLeft = this.transArr[index+1].left - rectInfo.left
-        let invertTop = this.transArr[index+1].top - rectInfo.top
-        const keyframes = [
-          {
-            transform: `translate(${invertLeft}px, ${invertTop}px)`,
-          },
-          { transform: "translate(0)" },
-        ]
-        const options = {
-          duration: 1000,
-          easing: "cubic-bezier(0,0,0.32,1)",
-        }
-        value.animate(keyframes, options)
-      });
-    }
+    console.log('activeCardList',activeCardList, this.transArr)
+    activeCardList.forEach((value, index) => {
+      let rectInfo = value.getBoundingClientRect();
+      let invertLeft = this.transArr[index].left - rectInfo.left
+      let invertTop = this.transArr[index].top - rectInfo.top
+      const keyframes = [
+        {
+          transform: `translate(${invertLeft}px, ${invertTop}px)`,
+        },
+        { transform: "translate(0)" },
+      ]
+      const options = {
+        duration: 1000,
+        easing: "cubic-bezier(0,0,0.32,1)",
+      }
+      value.animate(keyframes, options)
+    });
+  }
+  disorder = ()=>{
+    console.log(11)
   }
   componentDidMount() {
-    let a = Array(3).fill({
-      a:1,
-      b:2
-    });
-    console.log(a)
     let cardList = this.creatCardList(this.state.initCardNum);
     this.setState(
         {
